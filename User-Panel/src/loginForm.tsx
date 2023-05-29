583346abbde94fc327bf7cfe964b9056ca52c0f2
Sadet Turhan <msadetturhan1@gmail.com>
2023-05-29 08:41:17 +0300

    starting links and some tailwindcss

--- User-Panel/src/loginForm.tsx
+++ User-Panel/src/loginForm.tsx
@@ -11,26 +11,27 @@ export default function LoginForm() {
     const onSubmit = handleSubmit(data => console.log(data));
   
     return (
-      <form onSubmit={onSubmit}>
+      <div className="container mx-auto my-8 px-4 w-2/4 h-3/4">
+      <form className="bg-white shadow-md rounded w-full h-full px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
         <h1>MALWATION</h1>
         <h2>Log in to your account</h2>
         <h3>Welcome back! Please enter your details.</h3>
-        <input type="text" placeholder="e-mail" {...register("email", { required: true , pattern: /^\S+@\S+$/i})} />
-        <input type="text" placeholder="password" {...register("password", { required: true })} />
+        <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">E-mail</label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="e-mail" {...register("email", { required: true , pattern: /^\S+@\S+$/i})} />
+        </div>
+        <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
+        <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="******" {...register("password", { required: true })} />
+        </div>
         <a>Forgot your password?</a>
-        <button
-        type="button"
-        onClick={() => {
-        }}
-      >
-        Sign In
-      </button>
-        <h4>Don't have an account?<a>Sign Up</a></h4>
         <Link to="/users">
-     <button type="button">
-          Click Me!
-     </button>
-      </Link>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Sign In
+        </button>
+        </Link>  
+        <h4>Don't have an account?<a>Sign Up</a></h4>
       </form>
+      </div>
     );
   }
--- User-Panel/src/users.tsx
+++ User-Panel/src/users.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Outlet, Link } from "react-router-dom";
 
 class Users extends Component {
   constructor(props: string) {
@@ -7,8 +8,19 @@ class Users extends Component {
 
   render() {
     return (
-      <div>
-        <h1 className='header'>users</h1>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <h1>Users</h1>
+        <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-black-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+        <Link to="/">
+        <button type="button">
+          Sign Out
+        </button>
+        </Link>  
       </div>
     );
   }
