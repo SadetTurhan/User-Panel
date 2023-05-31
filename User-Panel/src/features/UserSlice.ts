a1d58e5903b013fdf9cff399f2a19a7a5f284926
Sadet Turhan <msadetturhan1@gmail.com>
2023-05-31 19:45:25 +0300

    working with redux

---
+++ User-Panel/src/features/UserSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+interface UserState {
+    value: string[];
+}
+
+const initialState: UserState = {
+    value: []
+}
+export const UserSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        addUser: (state, action:PayloadAction<string>) => {
+            state.value.push(action.payload)
+        },
+        removeUser: (state, action:PayloadAction<number>) => {
+            state.value.splice(action.payload, 1)
+        }
+    },
+})
+
+export const { addUser, removeUser} = UserSlice.actions
+
+export default UserSlice.reducer;
--- User-Panel/src/users.tsx
+++ User-Panel/src/users.tsx
@@ -1,20 +1,31 @@
+import React, { useState } from "react";
 import { Sidebar } from '/home/sadet/repos/User-Panel/User-Panel/src/sidebar';
 import data from "./users.json"
 import { Usertype } from "./usertypes";
+import UserCard from './components/UserSection';
+import { useSelector, useDispatch} from "react-redux";
+import { RootState } from "./store";
+import { addUser } from "./features/UserSlice";
 const user = data as Usertype[];
-console.log(user[0].id)
 function Users() {
-
+  const [userNameInput, setUserNameInput] = useState("")
+  const users = useSelector((state:RootState) => state.users.value)
+  const dispatch = useDispatch()
+  const handleAddUsers = () => {
+    if(!userNameInput) return;
+    dispatch(addUser(userNameInput));
+    setUserNameInput("")
+  }
     return (
       <div>
       <Sidebar></Sidebar>
       <div>
         <p className="text-4xl">Users</p>
-        <label>search</label>
-        <input></input>
-        <p>{user[0].name}{user[0].id}</p>
-        <p>{user[1].name}</p>
-        <p>{user[2].name}</p>
+        {users.map((name,index) => {
+                return <UserCard name={name} index={index} />
+                })}
+                <input value={userNameInput} onChange={(e) => setUserNameInput(e.target.value)}/>
+                <button onClick={handleAddUsers}>Add</button>
       </div>
       </div>
     );
